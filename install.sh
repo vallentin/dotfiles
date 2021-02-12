eb5dbe62f65d565ca7eddb36825950fca57e3321
vallentin <mail@vallentin.dev>
2021-02-12 19:39:59 +0100

    Added base aliases file

--- install.sh
+++ install.sh
@@ -6,7 +6,16 @@ dirname=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)
 cd "$dirname"
 
 cp -r bin ~
+cp aliases $HOME/.vallentin-aliases
 
 files=$(ls bin)
 cd ~/bin
 chmod +x $files
+
+source='source $HOME/.vallentin-aliases'
+if ! grep -q "$source" $HOME/.bashrc; then
+    echo >> $HOME/.bashrc
+    echo "$source" >> $HOME/.bashrc
+fi
+
+# Then execute `source $HOME/.bashrc`
