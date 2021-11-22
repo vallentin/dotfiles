f9e44551550e13a8690063e6ed568f04accb6809
vallentin <mail@vallentin.dev>
2021-11-22 09:30:26 +0100

    Updated to echo progress

--- install.sh
+++ install.sh
@@ -5,8 +5,12 @@ set -e
 dirname=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)
 cd "$dirname"
 
+echo "Copying files..."
+
 cp -r bin ~
 cp aliases $HOME/.vallentin-aliases
+
+echo "Marking as executable..."
 
 files=$(ls bin)
 cd ~/bin
@@ -14,8 +18,11 @@ chmod +x $files
 
 source='source $HOME/.vallentin-aliases'
 if ! grep -q "$source" $HOME/.bashrc; then
+    echo "Appending to `$HOME/.bashrc`"
+
     echo >> $HOME/.bashrc
     echo "$source" >> $HOME/.bashrc
 fi
 
-# Then execute `source $HOME/.bashrc`
+echo "Installed"
+echo "Restart the terminal or \`source \$HOME/.bashrc\`"
